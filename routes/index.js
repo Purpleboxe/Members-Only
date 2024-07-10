52a3a5719d2e59593b1b1c528bade7c6fa404214
Antonio Zamora <antoniozamora21331@gmail.com>
2024-07-10 16:33:38 -0700

    Update CSS, display messages, and change some of the existing views

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require("express");
 var router = express.Router();
 
+const Message = require("../models/message");
+
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Members Only", user: req.user });
+router.get("/", async function (req, res, next) {
+  const messages = await Message.find().populate("user").exec();
+  res.render("index", {
+    title: "Members Only",
+    user: req.user,
+    messages: messages,
+  });
 });
 
 module.exports = router;
--- routes/messages.js
+++ routes/messages.js
@@ -2,9 +2,9 @@ var express = require("express");
 var router = express.Router();
 
 const message_controller = require("../controllers/messageController");
-const { auth } = require("../config/auth");
+const { notAuth } = require("../config/auth");
 
-router.get("/create", auth, message_controller.message_create_get);
-router.post("/create", auth, message_controller.message_create_post);
+router.get("/create", notAuth, message_controller.message_create_get);
+router.post("/create", notAuth, message_controller.message_create_post);
 
 module.exports = router;
